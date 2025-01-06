f1db13fec71186e9fb8898002fc783f7f38169f6
Isai <isnavarro@estudiantec.cr>
2025-01-06 11:39:44 -0600

    Animacion de botones, creacion de botones en header, configuracin de biblioteca AutoAnimate e implementacion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
-
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
 import Card from './Components/Card.jsx';
 import CardsData from './assets/Data.js';
+import { useState } from 'react';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 function App() {
   const cards = CardsData();
+  const [activeCard, setActiveCard] = useState('quienSoy'); //Visibilidad por defecto
+  const [parent] = useAutoAnimate();
+
   return (
     <>
-    <Header/>
-    <div 
-      className="cards-wrapper">
-      {cards.map((card) => (
-      <Card key={card.id} data={card} />
-          )
-        )
+    <Header setActiveCard={setActiveCard}/>
+    <div ref={parent} className="cards-wrapper">
+      {cards.filter((card) => card.id === activeCard).map((card) => (<Card key={card.id} data={card} />))
       }
     </div>
     <Footer/>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,23 @@
 
-function Header(){
+function Header({ setActiveCard }){
 
     return(
         <div className="HContainer">
             <h1 className="Heading">Mi Viaje Como Desarrollador</h1>
             <nav className="Navegator">
                 <ul className="Card-Index">
-                    <li>Quien soy</li>
-                    <li>Educacion</li>
-                    <li>Proyectos</li>
-                    <li>Contactos</li>
+                <li>
+                    <button onClick={() => setActiveCard('quienSoy')}>Quien soy</button>
+                </li>
+                <li>
+                    <button onClick={() => setActiveCard('educacion')}>Educación</button>
+                </li>
+                <li>
+                    <button onClick={() => setActiveCard('proyectos')}>Proyectos</button>
+                </li>
+                <li>
+                    <button onClick={() => setActiveCard('contactos')}>Contactos</button>
+                </li>
                 </ul>
             </nav>
         </div>
